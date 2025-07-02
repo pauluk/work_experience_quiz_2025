5e6d27a2bf5e2c346903c59eb4db0c27d7f062ae
pauluk <likewisedesign@gmail.com>
2025-07-02 02:22:41 +0100

    Add Streamlit app and HTML file

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Read the HTML file content
+with open("/Users/I-Cloud/Documents/GitHub/work_experience_quiz_2025/Ice_breaker_Data_Detectives_Case_Files.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Embed the HTML content in the Streamlit app
+components.html(html_content, height=1000, scrolling=True)
+
+st.write("This is your HTML content embedded in a Streamlit app.")
